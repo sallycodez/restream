73a54458e30aa5012956898751e6d11e8a262a15
MOUAD <dev.mouads.al@gmail.com>
2025-09-27 21:01:02 +0100

    Update restream.sh

--- restream.sh
+++ restream.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Replace with your IPTV URL and Livepush stream key
+# Replace this with your IPTV link
 IPTV_URL="http://ugeen.live:8080/Ugeen_VIPArFrgF/jhfJ51/126"
+
+# Livepush RTMP URL (Server + Stream Key)
 LIVEPUSH_URL="rtmp://stream.livepush.io/live/rtmp_28e57109040945e9a603a035bb5ea433"
 
+# Start streaming
 ffmpeg -re -i "$IPTV_URL" -c copy -f flv "$LIVEPUSH_URL"
+
+
